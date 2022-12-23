f3b6e41af4722238b4b371e8399d22af25333fa9
MilesV64 <milesv302@gmail.com>
2022-12-23 13:51:21 -0800

    use dummy UIKeyInput

--- KeyboardTests/ViewController.swift
+++ KeyboardTests/ViewController.swift
@@ -7,36 +7,55 @@
 
 import UIKit
 
+class DummyKeyInput: UIView, UIKeyInput {
+    
+    var hasText: Bool {
+        return false
+    }
+    
+    func insertText(_ text: String) {
+        
+    }
+    
+    func deleteBackward() {
+        
+    }
+    
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    override var canResignFirstResponder: Bool {
+        return true
+    }
+}
+
 class ViewController: UIViewController, UITextViewDelegate {
     
-    private let doneButton = UIButton(type: .system)
+    private var becomeFirstResponderTime: CFTimeInterval?
     
-    private let textView = UITextView()
-    
-    private var textViewDidBeginEditingTime: CFTimeInterval?
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.textView.text = DummyText.loremIpsum
-        self.textView.textColor = UIColor.label
-        self.textView.font = UIFont.systemFont(ofSize: 17, weight: .regular)
-        self.textView.textContainerInset = UIEdgeInsets(top: 32, left: 24, bottom: 32, right: 24)
-        self.textView.delaysContentTouches = false
-        self.textView.delegate = self
-        self.view.addSubview(self.textView)
+        let dummpyInputView = DummyKeyInput()
+        self.view.addSubview(dummpyInputView)
         
-        self.doneButton.setTitle("Done", for: .normal)
-        self.doneButton.contentHorizontalAlignment = .right
-        self.doneButton.addAction(UIAction(handler: { _ in
-            self.textView.resignFirstResponder()
-        }), for: .primaryActionTriggered)
-        self.view.addSubview(self.doneButton)
+        // cycles between becoming/resigning first responder
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+            if dummpyInputView.isFirstResponder {
+                dummpyInputView.resignFirstResponder()
+            }
+            else {
+                self.becomeFirstResponderTime = CACurrentMediaTime()
+                dummpyInputView.becomeFirstResponder()
+            }
+        })
         
         // keyboardWillChangeFrameNotification and keyboardWillShowNotification have nearly the same delay
         NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillChangeFrameNotification, object: nil, queue: .current, using: { _ in
             
-            guard let editingTime = self.textViewDidBeginEditingTime else { return }
+            guard let editingTime = self.becomeFirstResponderTime else { return }
             
             let elapsed = CACurrentMediaTime() - editingTime
             let milliseconds: Int = Int(elapsed * 1000)
@@ -45,45 +64,8 @@ class ViewController: UIViewController, UITextViewDelegate {
             // Xcode 14:      100-300s
             print("Elapsed: \(milliseconds)ms")
             
-            self.textViewDidBeginEditingTime = nil
+            self.becomeFirstResponderTime = nil
         })
     }
-    
-    func textViewShouldBeginEditing(_ textView: UITextView) -> Bool {
-        
-        // Set textViewDidBeginEditingTime to the current time so we can check how much time has passed
-        // until the keyboard notification fires.
-        self.textViewDidBeginEditingTime = CACurrentMediaTime()
-        
-        return true
-    }
-    
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        
-        self.doneButton.frame = CGRect(
-            x: self.view.bounds.maxX - 24 - 80,
-            y: self.view.safeAreaInsets.top,
-            width: 80,
-            height: 60
-        )
-        
-        self.textView.frame = self.view.bounds
-            .inset(by: UIEdgeInsets(top: self.doneButton.frame.maxY, left: 0, bottom: 0, right: 0))
-    }
-
-
-}
-
-struct DummyText {
-    
-    static let loremIpsum: String =
-"""
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-"""
     
 }
